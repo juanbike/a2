de02c5669df36aff4b6a5d2b65ea8b72bdd83c1f
juanbike <juancarlos.arteaga1@gmail.com>
2023-10-03 18:58:48 -0400

    proyecto entity

--- src/proyectos/entities/proyecto.entity.ts
+++ src/proyectos/entities/proyecto.entity.ts
@@ -1,1 +1,57 @@
-export class Proyecto {}
+/* eslint-disable prettier/prettier */
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+import {
+  IsNotEmpty,
+  IsString,
+  IsNumber,
+  Min,
+  IsPositive,
+  IsEmail,
+  IsPhoneNumber,
+} from 'class-validator';
+@Entity()
+export class Proyecto {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column('text', { nullable: true })
+  @Column({ length: 70 })
+  @IsNotEmpty()
+  @IsString()
+  nombreProyecto: string;
+
+  @Column('text', { nullable: true })
+  @Column({ length: 70 })
+  @IsNotEmpty()
+  @IsString()
+  Cliente: string;
+
+  @Column('text', { nullable: true })
+  @Column({ length: 70})
+  @IsNotEmpty()
+  @IsString()
+  @IsPhoneNumber()
+  titulo: string;
+
+  @Column('text', { nullable: true })
+  @Column({ length: 25 })
+  @IsNotEmpty()
+  @IsString()
+  @IsPhoneNumber()
+  revision: string;
+
+  @Column('text', { nullable: true })
+  @Column({ length: 60 })
+  @IsNotEmpty()
+  @IsString()
+  @IsPhoneNumber()
+  elaboradoPor: string;
+
+  @CreateDateColumn({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  fecha: Date;
+}
